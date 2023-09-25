2c90067b89f5cd4150660a0fcc410b3560dd4941
Danilo <106564033+1Danilo@users.noreply.github.com>
2023-09-25 16:13:31 -0300

    valor valido searchM()

--- main.js
+++ main.js
@@ -24,128 +24,115 @@ function genero() {
     }
 }
 
-function searchM(){
+function searchM() {
+  let busqueda;
   
-  let busqueda = parseInt(prompt("¿Que deseas buscar?  \n 1 - Remeras \n 2 - Pantalones \n 3 - Zapatillas  \n 4 - Accesorios \n 5 - Salir"))
+  do {
+    busqueda = parseInt(prompt("¿Qué deseas buscar?  \n 1 - Remeras \n 2 - Pantalones \n 3 - Zapatillas  \n 4 - Accesorios \n 5 - Salir"));
+    
+    if (isNaN(busqueda)) {
+      alert("Ingresa un valor válido.");
+    } else {
+      switch (busqueda) {
+        case 1: {
+          let remeras_m = parseInt(prompt("Elegiste Remeras, selecciona una opción: \n 1 - Remera Element Summon \n 2 - Remera DC Density Zone \n 3 - Remera Santa Cruz Chest Assorted"));
 
+          switch (remeras_m) {
+            case 1:
+              alert("Elegiste: Remera Element Summon");
+              break;
 
-  while (isNaN(busqueda) || busqueda === ""){
+            case 2:
+              alert("Elegiste: Remera DC Density Zone");
+              break;
 
-    busqueda = parseInt(prompt("Ingrese un valor válido"))
-  }
-  while (busqueda !== 5){
-    if (busqueda === 1){
-      let remeras_m = parseInt(prompt("Elegiste Remeras, seleccione una opción: \n 1 - Remera Element Summon \n 2 - Elegiste Remera DC Density Zone \n 3 - Elegiste Remera Santa Cruz Chest Assorted"))
+            case 3:
+              alert("Elegiste: Remera Santa Cruz Chest Assorted");
+              break;
 
-      switch(remeras_m){
-        case 1:{
-          alert("Elegiste: Remera Element Summon");
-          break;
-        }
-
-        case 2:{
-          alert("Elegiste: Remera DC Density Zone");
-          break;
-        }
-
-        case 3:{
-          alert("Elegiste: Remera Santa Cruz Chest Assorted");
-          break;
-        }
-
-        default:
-          alert("Opción NO Válida")
-          break;
-      }
-
-      break;
-
-    }
-
-    if (busqueda === 2){
-      let pantalones_m = parseInt(prompt("Elegiste Pantalones, seleccione una opción: \n 1 - Jogging Volcom Plain Melange \n 2 - Jogging Independent Baggy Bauhaus \n 3 - Pantalón Zoo York Bomber "))
-
-      switch (pantalones_m){
-        case 1:{
-          alert("Elegiste: Jogging Volcom Plain Melange ")
-          break;
-        }
-
-        case 2:{
-          alert("Elegiste: Jogging Independent Baggy Bauhaus")
-          break;
-        }
-
-        case 3:{
-          alert("Elegiste: Pantalón Zoo York Bomber")
-          break;
-        }
-
-        default:
-          alert("Opción NO Válida")
-          break;
-      }
-      
-      break;
-
-    }
-
-    if (busqueda === 3){
-      let zapatillas_m = parseInt(prompt("Elegiste Zapatillas, seleccione una opción: \n 1 - Zapatillas Vans Skate Old Skool LTH Pro \n 2 - Zapatillas DC Manteca SS"))
-
-      switch(zapatillas_m){
-        case 1:{
-          alert("Elegiste: Zapatillas Vans Skate Old Skool LTH Pro")
+            default:
+              alert("Opción NO Válida");
+          }
           break;
         }
 
         case 2: {
-          alert("Elegiste: Zapatillas DC Manteca SS")
+          let pantalones_m = parseInt(prompt("Elegiste Pantalones, selecciona una opción: \n 1 - Jogging Volcom Plain Melange \n 2 - Jogging Independent Baggy Bauhaus \n 3 - Pantalón Zoo York Bomber"));
+
+          switch (pantalones_m) {
+            case 1:
+              alert("Elegiste: Jogging Volcom Plain Melange");
+              break;
+
+            case 2:
+              alert("Elegiste: Jogging Independent Baggy Bauhaus");
+              break;
+
+            case 3:
+              alert("Elegiste: Pantalón Zoo York Bomber");
+              break;
+
+            default:
+              alert("Opción NO Válida");
+          }
           break;
         }
 
+        case 3: {
+          let zapatillas_m = parseInt(prompt("Elegiste Zapatillas, selecciona una opción: \n 1 - Zapatillas Vans Skate Old Skool LTH Pro \n 2 - Zapatillas DC Manteca SS"));
+
+          switch (zapatillas_m) {
+            case 1:
+              alert("Elegiste: Zapatillas Vans Skate Old Skool LTH Pro");
+              break;
+
+            case 2:
+              alert("Elegiste: Zapatillas DC Manteca SS");
+              break;
+
+            default:
+              alert("Opción NO Válida");
+          }
+          break;
+        }
+
+        case 4: {
+          let accesorios_m = parseInt(prompt("Elegiste Accesorios, selecciona una opción: \n 1 - Reloj \n 2 - Billetera \n 3 - Gorra \n 4 - Cinturón"));
+
+          switch (accesorios_m) {
+            case 1:
+              alert("Elegiste: Reloj");
+              break;
+
+            case 2:
+              alert("Elegiste: Billetera");
+              break;
+
+            case 3:
+              alert("Elegiste: Gorra");
+              break;
+
+            case 4:
+              alert("Elegiste: Cinturón");
+              break;
+
+            default:
+              alert("Opción NO Válida");
+          }
+          break;
+        }
+
+        case 5:
+          alert("Gracias por usar nuestro servicio. ¡Hasta luego!");
+          break;
+
         default:
-          break;
-
+          alert("Opción NO Válida");
       }
-
-      break;
-
     }
-
-    if (busqueda === 4){
-      let accesorios_m = parseInt(prompt("Elegiste Accesorios, seleccione una opción: \n 1 - Reloj \n 2 - Billetera \n 3 - Gorra \n 4 - Cinturón"))
-
-      switch (accesorios_m) {
-        case 1:
-          alert("Elegiste: Reloj")
-          break;
-
-        case 2:
-          alert("Elegiste: Billetera")
-          break;
-
-        case 3:
-          alert("Elegiste: Gorra")
-          break;
-
-        case 4:
-          alert("Elegiste: Cinturón")
-          break;
-      
-        default:
-          break;
-      }
-
-      break;
-
-    }
-
-
-  }
-
-
+  } while (busqueda !== 5);
 }
+
 
 function searchF(){
 
